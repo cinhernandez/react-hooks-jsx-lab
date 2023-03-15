6fe5f25a2f17d2ea6f91312ba45213bc51250163
Cindy Hernandez <cindyh@mail.com>
2023-03-15 07:34:24 -0700

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+ 
+   
+  return <div id="about me">
+    <h2>About Me</h2>
+    <p>Welcome Here!</p>
+   <img src={image} alt="I made this" />
+    </div>;
 }
 
 export default About;
